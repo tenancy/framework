c77f087cfb2798eea31baf63b766e4c6169a6036
ArlonAntonius <37894143+ArlonAntonius@users.noreply.github.com>
2019-02-16 20:27:20 +0100

    Remove Config Dependency

--- src/Tenancy/resources/config/tenancy.php
+++ src/Tenancy/resources/config/tenancy.php
@@ -59,10 +59,5 @@ return [
         'auto-update' => env('TENANCY_DATABASE_AUTO_UPDATE', true),
 
         'auto-delete' => env('TENANCY_DATABASE_AUTO_DELETE', true),
-
-        /**
-         * Specify a Password Generator used for tenant database handling
-         */
-        'password-generator' => PasswordGenerator::class,
     ]
 ];
