3878ce4df0231cfeda291894d50eb91673b13f0e
Arlon Antonius <37894143+ArlonAntonius@users.noreply.github.com>
2019-02-23 21:15:19 +0100

    StyleCI Fixes

--- src/Database/PasswordGenerator/PasswordGenerator.php
+++ src/Database/PasswordGenerator/PasswordGenerator.php
@@ -4,8 +4,10 @@ namespace Tenancy\Database\Generators;
 
 use Tenancy\Database\Contracts\ProvidesPassword;
 
-class PasswordGenerator implements ProvidesPassword {
-    public function generate(Tenant $tenant) : string {
+class PasswordGenerator implements ProvidesPassword
+{
+    public function generate(Tenant $tenant) : string
+    {
         md5(sprintf(
             '%s.%s',
             $tenant->getTenantKey(),
